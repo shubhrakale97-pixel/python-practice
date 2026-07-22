5f6fb9463bec0061d3518740a8a26895158b5db4
shubhrakale97-pixel <shubhrakale97@gmail.com>
2026-07-22 21:02:03 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======== RESTART: C:/Users/DELL/Documents/python practice/calculator.py ========
+Enter first number:16
+Enter second number:20
+Addition= 36
+Substraction= -4
+Multiplication= 320
+Division= 0.8
